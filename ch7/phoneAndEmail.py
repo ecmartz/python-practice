4d4e29b3d2bb5eda5b27607adff6dcdb0e15d497
ecmartz <ecmartz.nm@gmail.com>
2016-02-21 10:47:17 -0800

    check both regexs in clipboard

--- ch7/phoneAndEmail.py
+++ ch7/phoneAndEmail.py
@@ -13,14 +13,23 @@ if __name__ == '__main__':
     (\s*(ext|x|ext.)\s*(\d{2,5}))?
     )''', re.VERBOSE)
     
-#TODO: Create email regex
-emailRegex = re.compile(r'''
+#Create email regex
+    emailRegex = re.compile(r'''
     [a-zA-Z0-9._%+-]+
     @
     [a-zA-Z0-9.-]+
     (\.[a-zA-Z]{2,4aaa})
     )''', re.VERBOSE)
 
-#TODO: Find matches in clipboard text.
+#Find matches in clipboard text.
+    text = str(pyperclip.paste())
+    matches = []
+    for groups in phoneRegex.findall(text)
+        phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+        if groups[8] != '':
+            phoneNum += ' x' + groups[8]
+        matches.append(phoneNum)
+    for groups in emailRegex.findall(text):
+        matches.append(groups[0])
 
 #TODO: Copy results to the clipboard.
