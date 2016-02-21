4fb77bb947a51c576da0ecd511f1d06295ac3e0d
ecmartz <ecmartz.nm@gmail.com>
2016-02-21 11:08:12 -0800

    copy results to clipboard

--- ch7/phoneAndEmail.py
+++ ch7/phoneAndEmail.py
@@ -14,17 +14,17 @@ if __name__ == '__main__':
     )''', re.VERBOSE)
     
 #Create email regex
-    emailRegex = re.compile(r'''
+    emailRegex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+
     @
     [a-zA-Z0-9.-]+
-    (\.[a-zA-Z]{2,4aaa})
+    (\.[a-zA-Z]{2,4})
     )''', re.VERBOSE)
 
 #Find matches in clipboard text.
     text = str(pyperclip.paste())
     matches = []
-    for groups in phoneRegex.findall(text)
+    for groups in phoneRegex.findall(text):
         phoneNum = '-'.join([groups[1], groups[3], groups[5]])
         if groups[8] != '':
             phoneNum += ' x' + groups[8]
@@ -32,4 +32,10 @@ if __name__ == '__main__':
     for groups in emailRegex.findall(text):
         matches.append(groups[0])
 
-#TODO: Copy results to the clipboard.
+#Copy results to the clipboard.
+    if len(matches) > 0:
+        pyperclip.copy('\n'.join(matches))
+        print('Copied to clipboard.')
+        print('\n'.join(matches))
+    else:
+        print('No phone numbers or emails found.')
