03f4e016663d90d63bed146d64e496d71942c178
ecmartz <ecmartz.nm@gmail.com>
2016-02-25 00:15:09 -0800

    Finish capitals

---
+++ ch8/randomQuizGenerator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import random
+
+# Quiz data
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock',
+    'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 
+    'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield',
+    'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort',
+    'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 
+    'Michigan': 'Lansing'}
+    # TODO: Finish capitals.
+    
+    # TODO: Create the quiz and answer key files.
+    
+    # TODO: Write out the header for the quiz.
+    
+    # TODO: Shuffle the order of the states.
+    
+    # TODO: Loop through all 50 states, making a question for each.
