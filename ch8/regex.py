9ddc1b98a0bc95dc2d8a51911d1a4d1d0161de62
ecmartz <ecmartz.nm@gmail.com>
2016-05-05 01:03:59 -0700

    Match any .txt file in hardcoded directory

---
+++ ch8/regex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import re
+from pprint import pprint
+
+
+if __name__ == '__main__':
+    path_to_open='/home/paco/workspace/python-practice/ch8/'
+    txtRegex = re.compile(r'.*\.txt')
+
+    contents = os.listdir(path_to_open)
+    myin = str(sys.argv[1])
+    theRegex = re.compile(myin)
+    
+    for i in contents:
+        if txtRegex.match(i):
+            with open(i,'r') as fandle:
+                line = fandle.read()
+                if theRegex.search(line):
+                    print("Found match in {}: {}".format(i, theRegex.search(line).group(0)))
