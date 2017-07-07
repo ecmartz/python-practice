2a2cfa198703bfa1fcb4209253490ff85d293ffd
Esteban Martinez <ecmartz.nm@gmail.com>
2017-07-06 22:52:09 -0700

    added 2048 scaffold

---
+++ ch11/2048.py
@@ -0,0 +1,12 @@
+#!python3
+
+# Play the game 2048 automatically
+#   Up, Right, Down, Left
+#   https://gabrielecirulli.github.io/2048/
+
+from selenium import webdriver
+
+# TODO: Open the browser to the site
+
+
+# TODO: Pass the right keystrokes
