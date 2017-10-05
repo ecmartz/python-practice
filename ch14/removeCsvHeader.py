f975489df28985fe4f36ce5c99f9b0b8f8767d82
Esteban Martinez <ecmartz.nm@gmail.com>
2017-10-04 21:54:44 -0700

    easy csv stuff

---
+++ ch14/removeCsvHeader.py
@@ -0,0 +1,29 @@
+#! python3
+# removeCsvHeader.py - Removes the header from all CSV files in the current working directory.
+
+import csv, os
+
+os.makedirs('headerRemoved', exist_ok=True)
+
+# Loop through every file in the current working directory.
+for csvFilename in os.listdir('.'):
+    if not csvFilename.endswith('.csv'):
+        continue
+
+    print('Removing header from %s...' % (csvFilename))
+    # Read the CSV file in (skipping first row).
+    csvRows = []
+    csvFileObj = open(csvFilename)
+    readerObj = csv.reader(csvFileObj)
+    for row in readerObj:
+        if readerObj.line_num == 1:
+            continue # Skip first row
+        csvRows.append(row)
+    csvFileObj.close()
+
+    # Write out the csv file
+    csvFileObj = open(os.path.join('headerRemoved', csvFilename), 'w', newline='')
+    csvWriter = csv.writer(csvFileObj)
+    for row in csvRows:
+        csvWriter.writerow(row)
+    csvFileObj.close()
